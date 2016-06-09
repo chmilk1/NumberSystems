8dbc5e2c5dfd264b0830055a0689e42eaace61a4
League Mac <league@WTS15.attlocal.net>
2016-06-08 18:31:03 -0700

    aa

--- src/Decimal.java
+++ src/Decimal.java
@@ -1,12 +1,14 @@
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
 public class Decimal {
 	private String input;
 	private int number;
-	private StringBuilder output = new StringBuilder();
+	private StringBuilder output;
+	
 	private static String MESSAGE = "Input Your Number";
 	private PrintWriter pw;
 
@@ -34,35 +36,63 @@ public class Decimal {
 	}
 
 	private void toHex() {
-
-		boolean let = false;
-		int letn;
-		char[] numbers = input.toCharArray();
-		for (int i=0;i<numbers.length;i++) {
-			if (Integer.parseInt(""+numbers[i]) > 6) {
-				output.append(numbers[i]);
-			} else if(Integer.parseInt(""+numbers[i]) != 1 && Integer.parseInt(""+numbers[i+1]) != 1){
-				output.append(numbers[i]);
-			} else if (Integer.parseInt(""+numbers[i]) != 1 && Integer.parseInt(""+numbers[i+1]) == 1) {
-				let = true;
-				letn = Integer.parseInt(""+numbers[i]);
-			} else  if (let){
-				let = false;
-				int ccode = (numbers[i-1]-48)+65;
-				output.append((char) ccode);
-
+		output = new StringBuilder();
+		ArrayList<String> ret = new ArrayList<String>();
+		int x = number;
+		int r = 0;
+		while(x!=0){
+			r= x%16;
+			x=x/16;
+			switch(r){
+			case 10:
+				ret.add("A");
+				break;
+			case 11:
+				ret.add("B");
+				break;
+			case 12:
+				ret.add("C");
+				break;
+			case 13:
+				ret.add("D");
+				break;
+			case 14:
+				ret.add("E");
+				break;
+			case 15:
+				ret.add("F");
+				break;
+			default:
+				ret.add(""+r);
+				
 			}
 		}
+		for (int i = ret.size(); i > 0; i--) {
+			output.append(ret.get(i-1));
+		}
+		input = output.toString();
+			
 	}
 
 	private void outBin() {
-		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Your Awnser Is: " + output.toString());
 
 	}
 
 	private void toBin() {
-		// TODO Auto-generated method stub
-
+		output = new StringBuilder();
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		int x = number;
+			for (int i = 32; i > 0; i--) {
+				if (x-(2^i)>=0) {
+					ret.add(1);
+				} else {
+					ret.add(0);
+				}
+			}
+			for (int i = ret.size(); i < 0; i++) {
+				output.append(ret.get(i));
+			}
 	}
 
 	private void inputDec() {
